d45c6c359d07c1661a64707b0718a692f0ac0c41
RobinEatingWorm <gavin.pu@mail.utoronto.ca>
2024-11-26 06:30:11 +0000

    Extend plotting functionality based on reviews

--- R/plot.R
+++ R/plot.R
@@ -28,9 +28,10 @@
 #'   the plot. This argument takes either a vector or list of vectors as input.
 #'   The output of `find_bouts()` can be passed into this argument, which will
 #'   display bouts in green and peaks in red by default.
-#' @param hlines y-values of horizontal lines to be added to the plot. This is
-#'   intended to display the `min_distance` argument from `find_bouts()` so that
-#'   users can see how bouts and peaks were separated.
+#' @param hlines numeric values indicating where horizontal lines will be
+#'   plotted on the y-axis. This is intended to display the `min_distance`
+#'   argument from `find_bouts()` so that users can see how bouts and peaks were
+#'   separated.
 #' @param time_min the minimum time to plot.
 #' @param time_max the maximum time to plot.
 #' @param ylim the y-limits of the plot. This can be useful to zoom closer to a
@@ -38,6 +39,10 @@
 #' @param col_time_indices colors of each vector of indices in the
 #'   `time_indices` argument.
 #' @param col_hlines colors of each horizontal line in the `hlines` argument.
+#' @param legend_default whether to display a default legend. This argument is
+#'   `TRUE` by default and assumes that the output of `find_bouts()` has been
+#'   passed into the `time_indices` argument. Otherwise, this argument should be
+#'   set to `FALSE` to avoid the creation of an erroneous legend.
 #'
 #' @examples
 #' trajectory <- create_trajectory(cadmium_zebrafish,
@@ -57,7 +62,7 @@
 #'                  time_min = 1240, time_max = 1260)
 #'
 #' @export
-#' @importFrom graphics abline points segments
+#' @importFrom graphics abline legend points segments
 plot_diagnostics <- function(trajectory,
                              main = "Distance Traveled Since Previous Time",
                              xlab = "Time",
@@ -68,23 +73,12 @@ plot_diagnostics <- function(trajectory,
                              time_max = max(trajectory$t),
                              ylim = NULL,
                              col_time_indices = c("green", "red"),
-                             col_hlines = c("blue")) {
+                             col_hlines = c("blue"),
+                             legend_default = TRUE) {
   # Validate arguments
   validate_trajectory(trajectory)
-  if (!is.null(time_indices)) {
-    if (is.numeric(time_indices)) {
-      validate_arg(time_indices, "time_indices", "numeric", length_one = FALSE)
-    } else {
-      for (i in seq_len(length(time_indices))) {
-        validate_arg(time_indices[[i]],
-                     paste0("time_indices[[", i, "]]"),
-                     "numeric",
-                     length_one = FALSE)
-      }
-    }
-  }
-  validate_arg(time_min, "time_min", "numeric", positive = FALSE)
-  validate_arg(time_max, "time_max", "numeric", positive = FALSE)
+  validate_diagnostic_time_indices(time_indices, col_time_indices)
+  validate_time_limits(time_min, time_max)
 
   # Calculate distances traveled between consecutive points
   p1 <- as.matrix(trajectory[1:(nrow(trajectory) - 1), c("x", "y")])
@@ -108,25 +102,34 @@ plot_diagnostics <- function(trajectory,
                      trajectory$t[(time_min_index + 1):time_max_index],
                      shifted_distances[(time_min_index + 1):time_max_index])
 
-  # Plot time indices of interest
-  if (is.list(time_indices)) {
-    for (i in seq_len(length(time_indices))) {
-      valid_time_indices <- time_indices[[i]] >= time_min_index &
-                            time_indices[[i]] <= time_max_index
-      time_indices[[i]] <- time_indices[[i]][valid_time_indices]
-      graphics::points(trajectory$t[time_indices[[i]]],
-                       shifted_distances[time_indices[[i]]],
-                       cex = 1.25, col = col_time_indices[i], pch = 4)
+  # Add a default legend if requested
+  if (legend_default) {
+    if (is.list(time_indices)) {
+      graphics::legend("topright", legend = c("Bouts", "Peaks"),
+                       col = col_time_indices,
+                       lty = 0, lwd = 2, pch = 4, pt.cex = 1.25)
+    } else if (is.numeric(time_indices)) {
+      graphics::legend("topright", legend = "Bouts",
+                       col = col_time_indices[1],
+                       lty = 0, lwd = 2, pch = 4, pt.cex = 1.25)
     }
-  } else if (is.numeric(time_indices)) {
-    valid_time_indices <- time_indices >= time_min_index &
-                          time_indices <= time_max_index
-    time_indices <- time_indices[valid_time_indices]
-    graphics::points(trajectory$t[time_indices],
-                     shifted_distances[time_indices],
-                     cex = 1.25, col = col_time_indices[1], pch = 4)
   }
 
+  # Plot time indices of interest
+  if (is.list(time_indices)) {
+    col_time_indices <- rep(col_time_indices,
+                            times = sapply(time_indices, length))
+    time_indices <- unlist(time_indices, use.names = FALSE)
+  } else if (is.numeric(time_indices)) {
+    col_time_indices <- rep(col_time_indices[1], times = length(time_indices))
+  }
+  valid_time_indices <- time_indices >= time_min_index &
+                        time_indices <= time_max_index
+  time_indices <- time_indices[valid_time_indices]
+  col_time_indices <- col_time_indices[valid_time_indices]
+  graphics::points(trajectory$t[time_indices],
+                   shifted_distances[time_indices],
+                   cex = 1.25, col = col_time_indices, lwd = 2, pch = 4)
 
   # Add horizontal lines of interest
   if (!is.null(hlines)) {
@@ -162,7 +165,8 @@ plot_diagnostics <- function(trajectory,
 #'                                 x = "X.center",
 #'                                 y = "Y.center",
 #'                                 t = "Trial.time")
-#' angles <- calculate_angles(trajectory)
+#' bouts <- find_bouts(trajectory, min_distance = 2, min_time = 0.6)
+#' angles <- calculate_angles(trajectory, bouts = bouts)
 #' plot_distribution(angles, "histogram",
 #'                   main = "Distribution of Angles",
 #'                   xlab = "Angle (degrees)",
@@ -171,7 +175,7 @@ plot_diagnostics <- function(trajectory,
 #'                   xlim = c(-180, 180))
 #'
 #' @export
-#' @importFrom graphics hist
+#' @importFrom graphics box hist
 #' @importFrom stats density ecdf
 plot_distribution <- function(observations,
                               type,
@@ -191,6 +195,7 @@ plot_distribution <- function(observations,
   if (type == "histogram") {
     graphics::hist(observations, breaks = breaks, freq = FALSE,
                    main = main, xlab = xlab, ylab = ylab, xlim = xlim)
+    graphics::box(bty = "o")
   } else if (type == "kde") {
     plot(stats::density(observations),
          main = main, xlab = xlab, ylab = ylab, xlim = xlim)
@@ -218,9 +223,18 @@ plot_distribution <- function(observations,
 #' @param ylab a label for the y-axis.
 #' @param time_min the minimum time to plot.
 #' @param time_max the maximum time to plot.
-#' @param col colors of each point in the plot. By default the rainbow color
-#'   palette is used, with red indicating the position at the earliest time and
-#'   magenta indicating the position at the latest time.
+#' @param col colors of each point in the plot. By default, this function uses
+#'   the rainbow color palette, which contains the colors red, yellow, green,
+#'   cyan, blue, and magenta in that order. Hues in between each consecutive
+#'   pair of colors are also included. The position colored in red occurs at the
+#'   earliest time, the position colored in yellow occurs at a time after the
+#'   time of the position colored in red, the position colored in green occurs
+#'   at a time after the time of the position colored in yellow, and so on, with
+#'   the position colored in magenta occurring at the latest time.
+#'
+#' @param time_colorbar whether to display a colorbar showing the mapping
+#'   between times and colors of points in the trajectory.
+#' @param time_colorbar_lab a label for the colorbar.
 #'
 #' @examples
 #' trajectory <- create_trajectory(cadmium_zebrafish,
@@ -229,10 +243,11 @@ plot_distribution <- function(observations,
 #'                                 t = "Trial.time")
 #' plot_trajectory(trajectory,
 #'                 xlab = "x-position (mm)", ylab = "y-position (mm)",
-#'                 time_min = 1200, time_max = 1260)
+#'                 time_min = 1200, time_max = 1260,
+#'                 time_colorbar_lab = "Time\n(s)")
 #'
 #' @export
-#' @importFrom graphics segments
+#' @importFrom graphics image layout par segments
 #' @importFrom grDevices rainbow
 plot_trajectory <- function(trajectory,
                             main = "Trajectory",
@@ -240,11 +255,17 @@ plot_trajectory <- function(trajectory,
                             ylab = "y-position",
                             time_min = min(trajectory$t),
                             time_max = max(trajectory$t),
-                            col = NULL) {
+                            col = NULL,
+                            time_colorbar = TRUE,
+                            time_colorbar_lab = "Time") {
   # Validate arguments
   validate_trajectory(trajectory)
-  validate_arg(time_min, "time_min", "numeric", positive = FALSE)
-  validate_arg(time_max, "time_max", "numeric", positive = FALSE)
+  validate_time_limits(time_min, time_max)
+
+  # Specify the plot arrangement if a colorbar for time will be added
+  if (time_colorbar) {
+    graphics::layout(matrix(c(1, 2), ncol = 2), widths = c(0.875, 0.125))
+  }
 
   # Find indices of minimum and maximum times to plot
   time_min_index <- min(which(trajectory$t >= time_min))
@@ -264,6 +285,31 @@ plot_trajectory <- function(trajectory,
                      trajectory$x[(time_min_index + 1):time_max_index],
                      trajectory$y[(time_min_index + 1):time_max_index],
                      col = col)
+
+  # Add a colorbar for time
+  if (time_colorbar) {
+    graphics::par(mar = c(5, 1, 4, 2) + 0.1)
+
+    # Interpolate times and find the color for each interpolated time
+    times <- trajectory$t[time_min_index:time_max_index]
+    times_interpolated <- seq(trajectory$t[time_min_index],
+                              trajectory$t[time_max_index],
+                              length.out = 1000)
+    time_differences <- matrix(rep(times_interpolated, times = length(times)),
+                               nrow = length(times_interpolated),
+                               ncol = length(times)) -
+                        matrix(rep(times, times = length(times_interpolated)),
+                               nrow = length(times_interpolated),
+                               ncol = length(times),
+                               byrow = TRUE)
+    time_differences[which(time_differences < 0)] <- NA
+    col_indices <- apply(time_differences, 1, which.min)
+
+    # Plot the colorbar
+    graphics::image(0, times_interpolated, matrix(col_indices, nrow = 1),
+                    col = col, xlab = time_colorbar_lab, useRaster = TRUE,
+                    xaxt = "n")
+  }
 
   # Plot is finished
   return(invisible(NULL))
